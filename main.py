cdb5829ccdf49e1cc080b9c43bb786b05ec4fb14
David Ruiz <113980722+davidruiiz@users.noreply.github.com>
2022-09-29 17:42:26 +0200

    Task4&5

--- main.py
+++ main.py
@@ -17,11 +17,19 @@ def preparation_time_in_minutes(number_of_layers):
 
     :param number_of_layers: introducir el número de capas de la lasaña
     :return: devulve cuanto tiempo (en minutos) ha tardado en prepararlo, teniendo en cuenta que son dos minutos más por capa añadida.
+        Esta función toma un número entero que representa el número de capas añadidas al plato,
+    calculando el tiempo de preparación utilizando un tiempo de 2 minutos por capa añadida.
     """
     return number_of_layers * PREPARATION_TIME
 
+def elapased_time_in_minutes(number_of_layers, elapsed_bake_time):
+    """Calcular el tiempo transcurrido.
 
-
-
-
-
+    :param number_of_layers: introducir el número de capas de la lasaña.
+    :param elapsed_baked_time introducir el tiempo de cocción transcurrido.
+    :return: devuelve el tiempo total transcurrido (en minutos) de cocción y preparación.
+    
+        Esta función toma dos números enteros que representan el número de capas de lasaña y el tiempo de horneado
+     y calcula el total de minutos transcurridos en la cocción de la lasaña.
+     """
+    return preparation_time_in_minutes(number_of_layers) + elapsed_bake_time
