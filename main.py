05bc7700e3a6a74c95067ad7673a158dea829dd2
David Ruiz <113980722+davidruiiz@users.noreply.github.com>
2022-09-29 17:19:53 +0200

    Task2

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
 #timepo que la lasaña debería estar en el horno acorde con el libro de cocina
 EXPECTED_BAKE_TIME = 40
+
+def bake_time_remaining(elapsed_bake_time):
+    
+    """     Calcular el tiempo de horneado restante. 
+    :param elapsed_bake_time: introducir el tiempo de horneado transcurrido.
+    :return: devuelve el tiempo de horneado restante (en minutos) derivado de 'EXPECTED_BAKE_TIME'
+
+    Función que toma los valores reales que ha estado la lasaña en el horno como argumento y devuelve cuántos minutos le quedan por hornear a la lasaña basado en el 'EXPECTED_BAKE_TIME'
+ """
+    
+    return EXPECTED_BAKE_TIME - elapsed_bake_time
+
+
